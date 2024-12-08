769c6c3d999f26bace9e99347f32044e24306d04
abhijeetpaul01@gmail.com <abhijeetpaul01@gmail.com>
2024-12-08 21:43:09 +0530

    Second commit

--- AzureDemo/Program.cs
+++ AzureDemo/Program.cs
@@ -1,4 +1,5 @@
 using AzureDemo.Data;
+using AzureDemo.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,25 +8,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AzureDemoDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDemoConnectionString")));
+
+//builder.Services.AddSingleton(
+//    provider =>
+//    {
+//        var configuration = provider.GetRequiredService<IConfiguration>();
+//        string connectionString = configuration["ConnectionString"];
+//        string queueName = "myqueue"; // Replace with your queue name
+//        return new AzureQueueService(configuration);
+//    });
+builder.Services.AddSingleton<AzureQueueService>();
+
 var app = builder.Build();
+    
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-app.UseRouting();
+        app.UseRouting();
 
-app.UseAuthorization();
+        app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+        app.Run();
+    
